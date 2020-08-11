39c4a4eef1c290dbf11e30cd9b3d469f1c2b13ed
loChris <44450518+loChris@users.noreply.github.com>
2020-08-11 15:37:04 -0400

    Added search bar to filter results and added styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
 		super();
 		this.state = {
 			monsters: [],
+			searchField: '',
 		};
 	}
 
@@ -16,10 +18,23 @@ class App extends Component {
 		);
 	}
 
+	handleChange = (e) => {
+		this.setState({ searchField: e.target.value });
+	};
+
 	render() {
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
 		return (
 			<div className="App">
-				<CardList monsters={this.state.monsters}></CardList>
+				<h1>Monsters Rolodex</h1>
+				<SearchBox
+					placeholder="search monsters"
+					handleChange={this.handleChange}
+				/>
+				<CardList monsters={filteredMonsters}></CardList>
 			</div>
 		);
 	}
